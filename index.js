4b2a15ac3d48dfe7b953005d19ecb70681dfb600
Dustin <dustinmath2121@outlook.com>
2022-01-06 13:58:10 -0600

    Reformat a few things

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-export function downloadSVGImage(svgElement, name, format) {
-    if (!svgElement || !name) {
+export function downloadSVGImage(svgElementId, name, format) {
+    if (!svgElementId || !name) {
         return;
     }
 
     if(!format) {
-        format = 'svg';
+        format = "svg";
     }
 
-    const viewBox = svgElement.getAttribute('viewBox');
-    const dimensionArr = viewBox.splite(' ');
+    const viewBox = svgElementId.getAttribute("viewBox");
+    const dimensionArr = viewBox.splite(" ");
     const width = parseInt(dimensionArr[2]);
-    const height = parseInt(dimentionArr[3]);
+    const height = parseInt(dimensionArr[3]);
 
-    // If you don't set the width and height of the svgElement... it won't work in Firefox. Go figure
-    svgElement.setAttribute('width',svgElement.clientWidth.toString());
-    svgElement.setAttribute('height', svgElement.clientHeight.toString());
+    // If you don"t set the width and height of the svgElement... it won"t work in Firefox. Go figure
+    svgElementId.setAttribute("width",svgElementId.clientWidth.toString());
+    svgElementId.setAttribute("height", svgElementId.clientHeight.toString());
 
-    const xml = new XMLSerializer().serializeToString(svgElement);
+    const xml = new XMLSerializer().serializeToString(svgElementId);
     const svgBit64 = window.btoa(xml);
-    const b64Start = 'data:image/svg+xml;base64,'
+    const b64Start = "data:image/svg+xml;base64,"
 
-    const canvas = document.createElement('canvas');
+    const canvas = document.createElement("canvas");
     canvas.width = width;
     canvas.height = height;
 
     const image = new Image();
     image.onload = () => {
-        canvas.getContext('2d').drawImage(image,0,0,width, height);
+        canvas.getContext("2d").drawImage(image,0,0,width, height);
         canvas.toBlob( blob => {
             const link = document.createElement("a");
             link.download = `${name}.${format}`;
             let url;
-            if (format === 'svg') {
+            if (format === "svg") {
                 format = "svg+xml"
             }
             canvas.toDataURL(`image/${format}`, 1);
